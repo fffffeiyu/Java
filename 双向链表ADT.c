8b5a0033b17b85432d5795d6f43be1e2d9b1d37b
feiyu <59243379+fffffeiyu@users.noreply.github.com>
2020-04-02 22:01:35 +0800

    Add files via upload

---
+++ 双向链表ADT.c
@@ -0,0 +1,36 @@
+	//2020.4.1记：截止今日双链表尚未学完，头插法与链表的删除等为学 
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+	//定义双链表
+typedef struct Node
+{
+	struct Node *prior;
+	struct Node *next;
+}
+	Node,*DLinkList;
+	
+	//建立双链表（尾插法）
+DLinkList DLinkListCreatT()
+{
+	Node *L, *p, *r;
+	L = (Node *)malloc(sizeof(Node));
+	L->next = NULL;
+	r = L;
+	r->next = NULL;
+	ElemType x;
+	while (scanf("%d,&x" != NULL));
+	{
+		p = (Node *)malloc(sizeof(Node));
+   		p->data = x;
+    	p->next = r->next;
+    	r->next = p;
+    	r = p;
+
+	}
+	r->next = NULL;
+	return L;
+ } 
+	
+	
